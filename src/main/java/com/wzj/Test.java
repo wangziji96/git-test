ae88e3a98111a48c9eadfd3097523518a55dfe0a
wzj <wzj@git.com>
2022-05-07 15:56:51 +0800

    first commit

---
+++ src/main/java/com/wzj/Test.java
@@ -0,0 +1,13 @@
+package com.wzj;
+
+/**
+ * @Author wzj
+ * @Date 2022/5/7 15:06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
