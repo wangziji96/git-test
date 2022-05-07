f085fa441c654dd70a4f102d6850413d91005797
wzj <wzj@git.com>
2022-05-07 17:00:29 +0800

    hot-fix first commit

--- src/main/java/com/wzj/Test.java
+++ src/main/java/com/wzj/Test.java
@@ -10,6 +10,7 @@ public class Test {
         System.out.println("hello world");
         System.out.println("hello world2");
         System.out.println("hello world3");
+        System.out.println("this is hot-fix branch");
     }
 
 }
